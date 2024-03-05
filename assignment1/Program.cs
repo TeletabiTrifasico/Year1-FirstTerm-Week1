bc37aaa367cfb8105949560b1e3e80044b7a4b04
TeletabiTrifasico <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:30:23 +0100

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Excersice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Your age is " + age);
+        }
+    }
+}
