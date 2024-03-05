59ab68c0c4fd065bfefd2ff004c15cfc61bfde9d
TeletabiTrifasico <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:32:56 +0100

    Add files via upload

---
+++ assignment3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Excercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //read the users name
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+
+            //read last name
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+
+            //display full name (first option)
+            Console.WriteLine("Your full name is " + firstName + " " + lastName);
+
+            //display full name (second option)
+            Console.WriteLine("Your full name is {0} {1}", firstName, lastName);
+
+            //display full name (third option)
+            Console.WriteLine($"Your full name is {firstName} {lastName}");
+        }
+    }
+}
