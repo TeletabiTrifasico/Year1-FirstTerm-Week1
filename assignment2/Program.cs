c1fc1b591e699e617f7d3d4e321dd4d4f3144988
TeletabiTrifasico <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:31:35 +0100

    Add files via upload

---
+++ assignment2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Excercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your age? ");
+            string ageInput = Console.ReadLine();
+            int age = int.Parse(ageInput) + 1;
+            Console.WriteLine("The next year you will be " + age + " years old.");
+        }
+    }
+}
