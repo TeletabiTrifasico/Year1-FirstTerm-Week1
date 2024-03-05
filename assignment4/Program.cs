753a8ee373f7437222432a28b0ecdda1b300c306
TeletabiTrifasico <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:35:21 +0100

    Add files via upload

---
+++ assignment4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Excercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            int result = number + number;
+                
+            result += 10;
+
+            result /= 2;
+
+            result -=  number;
+
+            Console.WriteLine($"Result is {result}");
+        }
+    }
+}
